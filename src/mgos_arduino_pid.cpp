4bd34040f9f57643f2e346132c212782e06432fd
mamanbudiman <5770472+mamanbudiman@users.noreply.github.com>
2018-12-13 19:31:19 +0700

    Update and rename mgos_arduino_DS3231.cpp to mgos_arduino_pid.cpp

---
+++ src/mgos_arduino_pid.cpp
@@ -0,0 +1,76 @@
+
+#include "mgos_arduino_pid.h"
+
+PID *mgos_PID_create(double* Input, double* Output, double* Setpoint,
+        double Kp, double Ki, double Kd, int POn, int ControllerDirection) {
+   return new PID(Input, Output, Setpoint, Kp, Ki, Kd, POn, ControllerDirection);                        
+}
+
+void mgos_PID_close(PID *pid) {
+  if (pid != nullptr) {
+    delete pid;
+    pid = nullptr;
+  }
+}
+
+bool mgos_PID_Compute(PID *pid) {
+  if (pid == nullptr) return false;
+  return pid->Compute();
+}
+  
+void mgos_PID_setTunings(PID *pid, double Kp, double Ki, double Kd, int POn) {
+  if (pid == nullptr) return;
+  pid->SetTunings(Kp, Ki, Kd, POn);
+}
+  
+void mgos_PID_SetSampleTime(PID *pid, int NewSampleTime) {
+  if (pid == nullptr) return;
+  pid->SetSampleTime(NewSampleTime);
+}
+
+void mgos_PID_SetOutputLimits(PID *pid, double Min, double Max) {
+  if (pid == nullptr) return;
+  pid->SetOutputLimits(Min, Max);
+}
+  
+void mgos_PID_SetMode(PID *pid, int Mode) {
+  if (pid == nullptr) return;
+  pid->SetMode(Mode);
+}
+  
+void mgos_PID_Initialize(PID *pid) {
+  if (pid == nullptr) return;
+  pid->Initialize();
+}   
+
+void PID::SetControllerDirection(int Direction)
+  
+void mgos_PID_SetControllerDirection(PID *pid, int Direction) {
+  if (pid == nullptr) return;
+  pid->SetControllerDirection(Direction);
+}
+
+double mgos_PID_GetKp(PID *pid) {
+  if (pid == nullptr) return 0;
+  return pid->GetKp();
+}
+
+double mgos_PID_GetKi(PID *pid) {
+  if (pid == nullptr) return 0;
+  return pid->GetKi();
+}
+
+double mgos_PID_GetKd(PID *pid) {
+  if (pid == nullptr) return 0;
+  return pid->GetKd();
+}
+    
+int mgos_PID_GetMode(PID *pid) {
+  if (pid == nullptr) return 0;
+  return pid->GetMode();
+}
+
+int mgos_PID_GetDirection(PID *pid) {
+  if (pid == nullptr) return 0;
+  return pid->GetDirection();
+}
